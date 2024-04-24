12f0a4d2bc2c385cc30a062ecbc5d69527db8b85
Zaki-byte-786 <146849204+Zaki-byte-786@users.noreply.github.com>
2024-04-24 20:16:57 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        System.out.println(arrayList);
+    }
+}
